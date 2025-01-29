fcfef8d08c0b01546a622f14180622f89ffdbddf
Nadia Tsygankova <nadiapritchina@gmail.com>
2025-01-29 14:32:39 -0500

    add audio playback functionality and update button styles for drum kit

--- projects/drum-kit/index.js
+++ projects/drum-kit/index.js
@@ -1,7 +1,14 @@
-
+let AudioList = ["crash", "kick-bass", "snare", "tom-1", "tom-2", "tom-3", "tom-4"];
+console.log(AudioList);
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-  document.querySelectorAll("button")[i].addEventListener("click", handleClick);
-}
-function handleClick(event) {
- console.log("I got clicked!");
+  document.querySelectorAll("button")[i].addEventListener("click", function () {
+    console.log("I got clicked!");
+    var buttonInnerHTML = this.style;
+    console.log(buttonInnerHTML);
+    let audio = new Audio( "./sounds/crash.mp3");
+    audio.play();
+  });
+
+
+
 }
