4efb34c28c6c3fbb34f390481363463cebe01e4e
Nadia Tsygankova <nadiapritchina@gmail.com>
2025-02-01 18:51:09 -0500

    implement game sequence logic and button flash effect in Simon Game

--- projects/simon_project/game.js
+++ projects/simon_project/game.js
@@ -1,7 +1,28 @@
-let buttonColours = ["red", "blue", "green", "yellow"];
+const buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
-let randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
+
 function nextSequence() {
+  // Generate random number between 0 and 3
   let randomNumber = Math.floor(Math.random() * 4);
+  // Use the random number to select a random colour from the buttonColours array
+  let randomChosenColour = buttonColours[randomNumber];
+  // Add the random colour to the game pattern
+  gamePattern.push(randomChosenColour);
+
+  // Flash the selected button
+  let button = $("#" + randomChosenColour);
+  button.addClass("flash");
+  playSound(randomChosenColour);
+  setTimeout(function () {
+      button.removeClass("flash");
+  }, 100);
 }
+function playSound(color) {
+  const audio = new Audio(`sounds/${color}.mp3`);
+  audio.play();
+}
+    // Trigger next sequence on click anywhere for testing
+    $(document).on("click", function () {
+      nextSequence();
+  });
+
