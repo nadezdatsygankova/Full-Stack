4a07b01156d0bf8ee66dcdb1e6f1d793e586092c
Nadia Tsygankova <nadiapritchina@gmail.com>
2025-02-03 18:41:34 -0500

    update URL in URL.txt and enhance index.js to save QR code image and URL to file

--- projects/Backend/QR Code Project/index.js
+++ projects/Backend/QR Code Project/index.js
@@ -10,7 +10,14 @@ inquirer
   }
   ])
   .then((answers) => {
-    console.log(answers);
+    const url = answers.URL;
+    const qr_png = qrImage.image(url);
+    qr_png.pipe(fs.createWriteStream("qr_img.png"));
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+
   })
   .catch((error) => {
     if (error.isTtyError) {
