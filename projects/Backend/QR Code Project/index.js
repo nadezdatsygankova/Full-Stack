4c0b120218ed078718ca8bf948433691b605a229
Nadia Tsygankova <nadiapritchina@gmail.com>
2025-02-03 18:30:57 -0500

    refactor QR Code project to use ES6 imports and add URL prompt functionality

--- projects/Backend/QR Code Project/index.js
+++ projects/Backend/QR Code Project/index.js
@@ -1,5 +1,21 @@
 
-const inquirer = require('inquirer');
-const qrImage = require('qr-image');
-const fs = require('fs');
+import fs from 'fs';
+import inquirer from 'inquirer';
+import qrImage from 'qr-image';
 
+inquirer
+  .prompt([{
+    message: "Type in your URL:",
+    name: "URL"
+  }
+  ])
+  .then((answers) => {
+    console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
