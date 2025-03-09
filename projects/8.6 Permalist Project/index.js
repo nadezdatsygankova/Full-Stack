4cfca79876b86240d47c9274fbfada03384d4561
Nadia Tsygankova <nadiapritchina@gmail.com>
2025-03-08 23:11:42 -0500

    feat: implement asynchronous database operations for add, edit, and delete item functionalities

--- projects/8.6 Permalist Project/index.js
+++ projects/8.6 Permalist Project/index.js
@@ -35,23 +35,37 @@ app.get("/", async (req, res) => {
   }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  // items.push({title: item});
+  try {
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/edit", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+
+  try {
+    await db.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/delete", (req, res) => {
+app.post("/delete", async (req, res) => {
   const id = req.body.deleteItemId;
-  items = items.filter((item) => item.id !==
-    parseInt(id));
-  res.redirect("/");
+  try {
+    await db.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 app.listen(port, () => {
