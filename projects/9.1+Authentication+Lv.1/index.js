762e629802de008f864a6ea7e685cab54bfdfbda
Nadia Tsygankova <nadiapritchina@gmail.com>
2025-03-12 15:27:31 -0400

    feat: implement user registration and login with PostgreSQL database integration

--- projects/9.1+Authentication+Lv.1/index.js
+++ projects/9.1+Authentication+Lv.1/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "Volgograd",
+  port: 5432,
+});
+db.connect();
+
+let currentUserId = null;
+
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
@@ -20,11 +33,31 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  console.log(req.body.username);
-  console.log(req.body.password);
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, password]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/login", async (req, res) => {});
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    const result = await db.query(
+      "SELECT id FROM users WHERE email = $1 AND password = $2 ORDER BY id ASC",
+      [email, password]);
+    if (result.rows.length > 0) {
+      currentUserId = result.rows[0].id;
+    }
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
